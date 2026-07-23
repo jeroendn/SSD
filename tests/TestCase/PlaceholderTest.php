66a1d77a5630d011298d4bbe9d7913d434956ffb
jeroendn <51778824+jeroendn@users.noreply.github.com>
2026-07-23 13:23:50 +0200

    Setup phpunit

---
+++ tests/TestCase/PlaceholderTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace SSD\Test\TestCase;
+
+use PHPUnit\Framework\TestCase;
+
+final class PlaceholderTest extends TestCase
+{
+    public function testPlaceholder(): void
+    {
+        $this->markTestIncomplete('No tests have been written yet.');
+    }
+}
